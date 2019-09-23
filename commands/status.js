3738236fcf6dc57d850719f8feb64843026efb41
Ian <hi@ian.pe>
2019-09-23 21:07:52 +0100

    Add status command

---
+++ commands/status.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'status',
+    description: 'Sets the bot status',
+    usage: '[status]',
+    cooldown: 0,
+    ownerOnly: true,
+    dmOnly: true,
+    execute(message, args) {
+        const status = args.join(' ');
+        message.client.user.setActivity(status);
+        message.channel.send(`Status set to  ${status}`);
+    }
+};
